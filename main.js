e8a852ab9f4a9dc67e2d1e7a32fc198f0d2d137b
Blobby2191 <87619169+Blobby2191@users.noreply.github.com>
2024-07-12 20:29:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var code = document.getElementById("password");
+
+function check () {
+    if (code = "💻🍴🎯") {
+        window.location.href = "zoom.html";
+    } else if (code = "➡️⚠️🥊") {
+        window.location.href = "email.html"
+    }
+}
